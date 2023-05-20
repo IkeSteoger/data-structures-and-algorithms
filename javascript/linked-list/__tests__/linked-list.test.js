57d7e9c0d94be938fa88b98b302b9c22b2f27e17
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-19 17:07:32 -0700

    linked list

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,76 @@
 'use strict';
 
-// Require our linked list implementation
 const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+
+  test('Can successfully instantiate an empty linked list', () => {
+    const list = new LinkedList();
+
+    expect(list.head).toBeNull();
+  });
+
+  test('Can properly insert into the linked list', () => {
+    const list = new LinkedList();
+    list.insert(1);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next).toBeNull();
+  });
+
+  test('The head property will properly point to the first node in the linked list', () => {
+    const list = new LinkedList();
+    list.insert(2);
+
+    expect(list.head.value).toEqual(2);
+    expect(list.head.next).toBeNull();
+
+    list.insert(1);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next).toBeNull();
+    list.append('a');
+    expect(list.head.next.next.value).toEqual('a');
+  });
+
+  test('Can properly insert multiple nodes into the linked list', () => {
+    const list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    expect(list.head.value).toEqual(3);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(1);
+    expect(list.head.next.next.next).toBeNull();
+  });
+
+  test('Will return true when finding a value within the linked list that exists', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.includes('b')).toEqual(true);
+  });
+
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.includes('g')).toEqual(false);
+  });
+
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+    list.append('l');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { l } -> NULL');
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,71 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
 }
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  insert(value){
+    let node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+
+  append(value){
+    let node = new Node(value);
+    if(!this.head){
+      this.head = node;
+      return;
+    }
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  // traversal(){
+  //   let current = this.head;
+  //   while(current){
+  //     console.log(current.value);
+  //     current = current.next;
+  //   }
+  // }
+
+  includes(value){
+    let current = this.head;
+    while(current){
+      if(current.value === value){
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  toString(){
+    let current = this.head;
+    let string = `{ ${ current.value } }`;
+    current = current.next;
+    while(current){
+      string = `${string} -> { ${current.value} }`;
+      current = current.next;
+    }
+    if (current === null) {
+      string = `${string} -> NULL`;
+    }
+    return string;
+  }
+}
+
+
 
 module.exports = LinkedList;
